f091211585037fe59bb32294c52c08fd2607917e
emddevelop <stadio18@hotmail.com>
2018-02-16 12:08:00 +0200

    Invoice improvements

--- public/site.js
+++ public/site.js
@@ -1,10 +1,25 @@
-console.log('inside side');
 
 function formatDate(dateToFormat){
     var monthsArray=[1,2,3,4,5,6,7,8,9,10,11,12];
     var invoiceDate=new Date(dateToFormat);
     var setDateToInput;
         setDateToInput=invoiceDate.getDate()+'-'+monthsArray[invoiceDate.getMonth()]+'-'+invoiceDate.getFullYear();
+
+    return setDateToInput;
+}
+
+function formatDateForInput(dateToFormat){
+    var monthsArray=[1,2,3,4,5,6,7,8,9,10,11,12];
+    var invoiceDate=new Date(dateToFormat);
+    var setDateToInput;
+    if(invoiceDate.getMonth()<10)
+        setDateToInput=invoiceDate.getFullYear()+'-0'+monthsArray[invoiceDate.getMonth()]+'-'+invoiceDate.getDate();
+    else if(invoiceDate.getDate()<10)
+        setDateToInput=invoiceDate.getFullYear()+'-'+monthsArray[invoiceDate.getMonth()]+'-0'+invoiceDate.getDate();
+    else if(invoiceDate.getMonth()<10 && invoiceDate.getDate()<10)
+        setDateToInput=invoiceDate.getFullYear()+'-0'+monthsArray[invoiceDate.getMonth()]+'-0'+invoiceDate.getDate();
+    else
+        setDateToInput=invoiceDate.getFullYear()+'-'+monthsArray[invoiceDate.getMonth()]+'-'+invoiceDate.getDate();
 
     return setDateToInput;
 }
